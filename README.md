38f7724daa16f9df2a21fd78541c6a24a5935135
kolvin <15124052+kolvin@users.noreply.github.com>
2022-10-10 23:35:56 +0000

    feat: init discord client with token and build app to repsond to a cheeky poke
    Signed-off-by: kolvin <15124052+kolvin@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 <!-- DON'T EDIT THIS SECTION, INSTEAD RE-RUN doctoc TO UPDATE -->
 ## Contents
 
-- [go-template](#go-template)
+- [boty-botterson](#boty-botterson)
 - [Prerequisites](#prerequisites)
-- [Run this bad boi](#run-this-bad-boi)
+- [Meet Boty Botterson](#meet-boty-botterson)
 
 <!-- END doctoc generated TOC please keep comment here to allow auto update -->
 
-# go-template
+# boty-botterson
 ![go](https://img.shields.io/badge/Go-v1.19-blue)
 [![Conventional Commits](https://img.shields.io/badge/Conventional%20Commits-1.0.0-%23FE5196?logo=conventionalcommits&logoColor=white)](https://conventionalcommits.org) 
 [![Semantic Release - angular](https://img.shields.io/static/v1?label=Semantic+Release&message=angular&color=e10079&logo=semantic-release)](https://github.com/semantic-release/semantic-release) [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
@@ -20,8 +20,6 @@
 
 ----
 
-# Run this bad boi
-1. `git clone <repo>`
-2. `cd <repo>`
-3. `cp .env.example`
-4. `docker-compose up`
+# Meet Boty Botterson
+
+![Alt text](.docs/boty-botterson.jpg?raw=true "env")
--- app/main.go
+++ app/main.go
@@ -1,7 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// Variables used for command line parameters
+var (
+	token string
+)
+
+func init() {
+	flag.StringVar(&token, "t", "", "Boty Token")
+	flag.Parse()
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+
+	if token == "" {
+		fmt.Println("No token provided. Please run: airhorn -t <bot token>")
+		return
+	}
+
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + token)
+	fmt.Println(token)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register the messageCreate func as a callback for MessageCreate events.
+	dg.AddHandler(messageCreate)
+
+	// In this example, we only care about receiving message events.
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+
+	fmt.Println("made it")
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
+
+type Gopher struct {
+	Name string `json: "name"`
+}
+
+// This function will be called (due to AddHandler above) every time a new
+// message is created on any channel that the authenticated bot has access to.
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	// This isn't required in this specific example but it's a good practice.
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!pokeboty" {
+		fmt.Println("Don fuckin' poke me, prick!")
+	}
 }
